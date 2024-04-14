bdacac92448142305e4a53e291776e3907d191f4
MEDIGONE <admin@medigo.one>
2024-04-14 18:59:23 +0200

    new approache

--- vivela/vivela/doctype/visa_non_lucratif/visa_non_lucratif.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Medigo and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Visa Non Lucratif", {
-// 	refresh(frm) {
-
-// 	},
-// });
