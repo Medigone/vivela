d913f5251917cb64ab862206f22a014bb9d874fb
MEDIGONE <admin@medigo.one>
2024-04-16 10:33:36 +0200

    fix: bouton creer

--- vivela/vivela/doctype/clients/clients.js
+++ vivela/vivela/doctype/clients/clients.js
@@ -22,3 +22,16 @@ function updateNomComplet(frm) {
     let completeName = (nom + " " + prenom).trim().toUpperCase();
     frm.set_value("nom_complet", completeName || null);
 }
+
+frappe.ui.form.on('Clients', {
+    refresh(frm) {
+        frm.add_custom_button(__('Dossier Médical'), function () {
+            // Logique pour créer une nouvelle commande client
+            frappe.new_doc('Dossier Medical', {
+                // Pré-remplissage de quelques champs si nécessaire
+                //prescripteur: frm.doc.name // Pré-remplir le nom du client dans la nouvelle commande client
+                // Vous pouvez ajouter d'autres champs ici
+            });
+        }, __('Create'));
+    }
+});
